bd741a87a43ff5672338bf599ae79723c82996e8
rlcngz <erolcengiz@gmail.com>
2022-08-18 16:00:11 +0200

    Serve up HTML and JSON object

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,17 +3,17 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello express");
+  res.send("<h1>Weather</h1>");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Help Page");
+  res.send([{ name: "Erol" }, { name: "Alona" }]);
 });
 app.get("/about", (req, res) => {
-  res.send("About Page");
+  res.send("<h1>About</h1>");
 });
 app.get("/weather", (req, res) => {
-  res.send("Weather Page");
+  res.send({ forecast: "Cloudy", location: "Amsterdam" });
 });
 
 app.listen(3000, () => {
