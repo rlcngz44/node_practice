552edb73f75c5ee5b6b67f7e59c9c05654c0c980
rlcngz <erolcengiz@gmail.com>
2022-09-05 14:23:59 +0200

    Create static directory for assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,12 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("<h1>Weather</h1>");
-});
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.get("/help", (req, res) => {
-  res.send([{ name: "Erol" }, { name: "Alona" }]);
-});
-app.get("/about", (req, res) => {
-  res.send("<h1>About</h1>");
-});
+app.use(express.static(publicDirectoryPath));
+
 app.get("/weather", (req, res) => {
   res.send({ forecast: "Cloudy", location: "Amsterdam" });
 });
